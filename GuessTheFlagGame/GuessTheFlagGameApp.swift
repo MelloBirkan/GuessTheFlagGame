abdec385487ab4d52507285832a66affa696a8b4
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2024-07-23 19:09:16 -0300

    Initial Commit

---
+++ GuessTheFlagGame/GuessTheFlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagGameApp.swift
+//  GuessTheFlagGame
+//
+//  Created by Marcello Gonzatto Birkan on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
